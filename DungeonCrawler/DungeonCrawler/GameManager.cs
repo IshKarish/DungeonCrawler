2269e71d4daef8f628a4ca80d9d8f5ce8b6a8b9e
Daniel Porat <danielporat5@gmail.com>
2024-02-28 14:51:58 +0200

    Move physics checks to it's own class

--- DungeonCrawler/DungeonCrawler/GameManager.cs
+++ DungeonCrawler/DungeonCrawler/GameManager.cs
@@ -22,10 +22,10 @@ public class GameManager
             game.UpdatePlayerLocation();
             map.PrintMap();
 
-            bool isCollidingFromRight = IsNextXColliding(player, navMesh, 1);
-            bool isCollidingFromLeft = IsNextXColliding(player, navMesh, -1);
-            bool isCollidingFromTop = IsNextYColliding(player, navMesh, -1);
-            bool isCollidingFromBottom = IsNextYColliding(player, navMesh, 1);
+            bool isCollidingFromRight = Physics.IsNextXColliding(player, navMesh, 1);
+            bool isCollidingFromLeft = Physics.IsNextXColliding(player, navMesh, -1);
+            bool isCollidingFromTop = Physics.IsNextYColliding(player, navMesh, -1);
+            bool isCollidingFromBottom = Physics.IsNextYColliding(player, navMesh, 1);
             
             ConsoleKeyInfo cki = Console.ReadKey();
             switch (cki.Key)
@@ -47,83 +47,7 @@ public class GameManager
                     if (!isCollidingFromLeft) player.MoveRight(-1);
                     break;
             }
-
-            //Console.WriteLine();
             Console.Clear();
         }
-    }
-
-    bool IsNextXColliding(Character player, NavMesh navMesh, int direction)
-    {
-        if (direction > 0) direction = 1;
-        else direction = -1;
-
-        int[] yPositions = CorrespondingYPositions(navMesh, player.Transform.Position.X, direction);
-
-        foreach (Vector2 w in navMesh.Walkable)
-        {
-            bool isNextXColliding = player.Transform.Position.X + direction == w.X;
-            if (isNextXColliding)
-            {
-                foreach (int y in yPositions)
-                {
-                    if (player.Transform.Position.Y == y) return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-
-    bool IsNextYColliding(Character player, NavMesh navMesh, int direction)
-    {
-        if (direction > 0) direction = 1;
-        else direction = -1;
-
-        int[] xPositions = CorrespondingXPositions(navMesh, player.Transform.Position.Y, direction);
-
-        foreach (Vector2 w in navMesh.Walkable)
-        {
-            bool isNextXColliding = player.Transform.Position.Y + direction == w.Y;
-            if (isNextXColliding)
-            {
-                foreach (int x in xPositions)
-                {
-                    if (player.Transform.Position.X == x) return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-
-    int[] CorrespondingXPositions(NavMesh navMesh, int yPosition, int direction)
-    {
-        if (direction > 0) direction = 1;
-        else direction = -1;
-        
-        List<int> x = new List<int>();
-        
-        foreach (Vector2 v in navMesh.Walkable)
-        {
-            if (v.Y == yPosition + direction) x.Add(v.X);
-        }
-
-        return x.ToArray();
-    }
-    
-    int[] CorrespondingYPositions(NavMesh navMesh, int xPosition, int direction)
-    {
-        if (direction > 0) direction = 1;
-        else direction = -1;
-        
-        List<int> y = new List<int>();
-        
-        foreach (Vector2 v in navMesh.Walkable)
-        {
-            if (v.X == xPosition + direction) y.Add(v.Y);
-        }
-
-        return y.ToArray();
     }
 }
---
+++ DungeonCrawler/DungeonCrawler/Physics.cs
@@ -0,0 +1,78 @@
+﻿namespace DungeonCrawler;
+
+public static class Physics
+{
+    public static bool IsNextXColliding(Character player, NavMesh navMesh, int direction)
+    {
+        if (direction > 0) direction = 1;
+        else direction = -1;
+
+        int[] yPositions = CorrespondingYPositions(navMesh, player.Transform.Position.X, direction);
+
+        foreach (Vector2 w in navMesh.Walkable)
+        {
+            bool isNextXColliding = player.Transform.Position.X + direction == w.X;
+            if (isNextXColliding)
+            {
+                foreach (int y in yPositions)
+                {
+                    if (player.Transform.Position.Y == y) return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    public static bool IsNextYColliding(Character player, NavMesh navMesh, int direction)
+    {
+        if (direction > 0) direction = 1;
+        else direction = -1;
+
+        int[] xPositions = CorrespondingXPositions(navMesh, player.Transform.Position.Y, direction);
+
+        foreach (Vector2 w in navMesh.Walkable)
+        {
+            bool isNextXColliding = player.Transform.Position.Y + direction == w.Y;
+            if (isNextXColliding)
+            {
+                foreach (int x in xPositions)
+                {
+                    if (player.Transform.Position.X == x) return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    static int[] CorrespondingXPositions(NavMesh navMesh, int yPosition, int direction)
+    {
+        if (direction > 0) direction = 1;
+        else direction = -1;
+        
+        List<int> x = new List<int>();
+        
+        foreach (Vector2 v in navMesh.Walkable)
+        {
+            if (v.Y == yPosition + direction) x.Add(v.X);
+        }
+
+        return x.ToArray();
+    }
+    
+    static int[] CorrespondingYPositions(NavMesh navMesh, int xPosition, int direction)
+    {
+        if (direction > 0) direction = 1;
+        else direction = -1;
+        
+        List<int> y = new List<int>();
+        
+        foreach (Vector2 v in navMesh.Walkable)
+        {
+            if (v.X == xPosition + direction) y.Add(v.Y);
+        }
+
+        return y.ToArray();
+    }
+}
